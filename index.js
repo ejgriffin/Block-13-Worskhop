4e3e9019d60c94f6de71abfdd379f3771cbe2a23
Eric Griffin <141835816+ejgriffin@users.noreply.github.com>
2023-09-25 12:17:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//Create if/else statements.
+// Use code if the value is truthy, print true;
+// If the value is falsy print different results;
+
+const inputValue1 = "I am s string";
+
+if (inputValue1) {
+  console.log(inputValue1 == true);
+} else {
+  console.log(false);
+}
+
+//I don't understand this at all.
