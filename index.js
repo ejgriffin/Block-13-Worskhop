c22f29bef24a5bbd1026b7ae549d12a141945562
Eric Griffin <141835816+ejgriffin@users.noreply.github.com>
2023-10-02 19:28:46 -0400

    Add files via upload
    second upload

--- index.js
+++ index.js
@@ -1,13 +1,320 @@
+//uncomment each section and run to test
+
+//Is Truthy:
+
 //Create if/else statements.
 // Use code if the value is truthy, print true;
-// If the value is falsy print different results;
+// If the value is falsy print different results:
+// "The boolean value false is falsy";
+// "The null value is falsy";
+// "undefined is falsy";
+// "The number 0 is falsy (the only falsy number)";
+// "The empty string is falsy (the only falsy string)";
 
-const inputValue1 = "I am s string";
+// let inputValue = "I am a string";
 
-if (inputValue1) {
-  console.log(inputValue1 == true);
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("The boolean value false is falsy");
+// }
+
+// let inputValue = false;
+
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("The boolean value false is falsy");
+// }
+
+// let inputValue = null;
+
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("The null value is falsy");
+// }
+
+// let inputValue = undefined;
+
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("undefined is falsy");
+// }
+
+// let inputValue = 0;
+
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("The number 0 is falsy (the only falsy number)");
+// }
+
+// let inputValue = "";
+
+// if (inputValue) {
+//   console.log(true);
+// } else {
+//   console.log("The empty string is falsy (the only falsy string)");
+// }
+
+//Number Line:
+
+// Using if/else if/else statements, JavaScript code in a script tag that takes the sum of two numbers and prints the corresponding result:
+
+// sum + " is less than -1000";
+// sum + " is a negative number";
+// sum + " is equal to 0";
+// sum + " is larger than 0";
+// sum + " is greater than 100";
+
+// Make variables to represent the input values and then a variable to represent the sum of those input values
+// code it up!
+
+// const num1 = 50;
+// const num2 = 51;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is larger than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "101 is greater than 100"
+
+// const num1 = 99;
+// const num2 = -2;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is greater than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "97 is greater than 0"
+
+// const num1 = 0;
+// const num2 = 101;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is greater than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "101 is greater than 100"
+
+// const num1 = 500;
+// const num2 = -500;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is greater than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "0 is equal to 0"
+
+// const num1 = -1000;
+// const num2 = 0;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is greater than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "-1000 is a negative number"
+
+// const num1 = -5;
+// const num2 = 0;
+// const theSum = num1 + num2;
+
+// if (theSum > 100) {
+//   console.log(`"${theSum} is greater than 100"`);
+// } else if (theSum < 0) {
+//   console.log(`"${theSum} is a negative number"`);
+// } else if (theSum == 0) {
+//   console.log(`"${theSum} is equal to 0"`);
+// } else if (theSum > 0) {
+//   console.log(`"${theSum} is greater than 0"`);
+// } else if (theSum < -1000) {
+//   console.log(`"${theSum} is less than -1000"`);
+// }
+// printed result: "-5 is a negative number"
+
+// Greater Than Or Equal To 5:
+
+// Using if/else statements, JavaScript code in a script tag that prints true if both parameters are greater or equal to 5.
+// Create variables for test values "num1" and "num2"
+//  Do the code!
+
+// const newNum1 = 5;
+// const newNum2 = 6;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// const newNum1 = 10;
+// const newNum2 = 11;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// const newNum1 = 0;
+// const newNum2 = 0;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: false
+
+// const newNum1 = 1000;
+// const newNum2 = -1000;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: false
+
+// const newNum1 = 6;
+// const newNum2 = 4;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: false
+
+// const newNum1 = 5;
+// const newNum2 = 5;
+
+// if (newNum1 >= 5 && newNum2 >= 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// Pair and Compare:
+
+// Using if/else statements and strict equality operators, JavaScript code in a script tag that compares two sets of two values.
+// Print true if at least one of the pairs is truthy.
+// create variables for the values.
+// use "or" operator ||
+
+// const param1A = "cat";
+// const param1B = "cat";
+// const param2A = 6;
+// const param2B = "6";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// const param1A = "five";
+// const param1B = 5;
+// const param2A = "dog";
+// const param2B = "dawg";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: false
+
+// const param1A = 0;
+// const param1B = false;
+// const param2A = "horse";
+// const param2B = "horse";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// const param1A = "eight";
+// const param1B = "eight";
+// const param2A = "ate";
+// const param2B = "ate";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: true
+
+// const param1A = 11;
+// const param1B = "eleven";
+// const param2A = "four";
+// const param2B = "for";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// printed result: false
+
+const param1A = "cake";
+const param1B = "cake";
+const param2A = "pie";
+const param2B = "pie";
+
+if (param1A === param1B || param2A === param2B) {
+  console.log(true);
 } else {
   console.log(false);
 }
-
-//I don't understand this at all.
+// printed result: true
